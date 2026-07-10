705e540f9aea4da646e5f361f82147235b34d0a9
Lopez Murillo, Luis Jair (L.) <brook_98@live.com>
2026-07-10 15:01:52 -0600

    initial commit

---
+++ src/main/java/com/test_container/spring_student_test_containers/SpringStudentTestContainersApplication.java
@@ -0,0 +1,13 @@
+package com.test_container.spring_student_test_containers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudentTestContainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudentTestContainersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test_container/spring_student_test_containers/SpringStudentTestContainersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test_container.spring_student_test_containers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudentTestContainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
